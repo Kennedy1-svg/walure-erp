f85acaf196ffdacab1a93e091676cb2ff8e105eb
Odumz <odumz20@gmail.com>
2022-02-21 19:44:27 +0100

    dashboard (course and student) views updated

---
+++ src/views/dashboard/Layout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Navigation from '../../components/SideNavigation.vue';
+import TopNavigation from '../../components/TopNavigation.vue';
+
+</script>
+
+<template>    
+    <div class="flex">
+        <Navigation class="w-1/5 h-full"/>
+        <div class="main w-4/5">
+            <TopNavigation />
+            <router-view />
+        </div>
+    </div>
+</template>
