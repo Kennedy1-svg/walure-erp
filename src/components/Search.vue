3f7112672836750177dbb2d9321f35beb61f5c82
Odumz <odumz20@gmail.com>
2022-02-21 01:37:21 +0100

    search, sidenav and topnav components updated

---
+++ src/components/Search.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import SvgIcons from './SvgIcons.vue'
+</script>
+
+<template>
+  <div class="flex items-center">
+    <div class="bg-white shadow px-4 p-2 flex mr-4 rounded-md">
+        <input class="rounded text-sm p-1 focus:outline-none" type="text" placeholder="Search">
+        <span class="w-auto flex justify-end items-center text-grey p-2">
+            <SvgIcons name="search"  />
+        </span>
+      </div>
+  </div>
+</template>
+
---
+++ src/components/TopNavigation.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import SvgIcons from '../components/SvgIcons.vue';
+</script>
+
+<template>
+<div class="flex bg-white shadow-md py-2 justify-end items-center px-16">
+	<div class="border text-grey flex justify-around gap-8 items-center rounded px-3 py-2">
+		<div class="icon border rounded-full ">
+			<SvgIcons class="m-2" name="user-avatar" /> <!-- avatar icon -->
+		</div>
+		<div class="text-black">
+			<p class="text-sm">Admin</p>
+			<p class="text-xs">Walure Capital</p>
+		</div>
+		<div class="icon">
+			<SvgIcons name="chevron-down" /> <!-- chevron down icon -->
+		</div>
+	</div>
+</div>
+</template>
