06deef43830a5be4a80dd79e7ad74e6377410168
Odumz <odumz20@gmail.com>
2022-02-27 19:35:01 +0100

    more components created

---
+++ src/components/alerts.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const props = defineProps({
+    name: String
+})
+</script>
+
+<template>
+    <div>
+        <div v-if="name == 'result'" class="flex items-center gap-4">
+            <div class="icon">
+                <slot name="icon"></slot>
+            </div>
+            <div class="info">
+                <slot name="info"></slot>
+            </div>
+            <div class="btn">
+                <slot name="button"></slot>
+            </div>
+        </div>
+        <div v-else-if="name == 'delete'">
+            <div class="bg-white p-7 rounded grid">
+                <div class="flex justify-between mb-6">
+
+                    <h1 class="text-xl mb-4 font-medium">
+                        <slot name="title">Delete Student</slot>
+                    </h1>
+                    <span>
+                        <SvgIcons class="cursor-pointer" onclick="document.getElementById('myModal').close();" name="o-cancel" />
+                    </span>
+                </div>
+                <p class="text-lg mb-10">
+                    <slot name="info">
+                        Are you sure you want to delete student?
+                    </slot>
+                </p>
+                <div class="flex justify-between items-center mb-3">
+                    <button class="px-10 py-4 rounded text-primary font-bold">
+                        Cancel
+                    </button>
+                    <button class="bg-red px-10 py-4 rounded text-white font-bold">
+                        <slot name="btn">
+                            Yes, Delete Student
+                        </slot>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/dashboard/main.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+import ActivityCard from '../ActivityCard.vue';
+</script>
+
+<template>
+    <div class="grid">
+        <div class="title">
+            <h1 class="text-xl font-bold">Recent Activity</h1>
+        </div>
+        <div class="main">
+            <div class="grid grid-cols-2 gap-5">
+                <ActivityCard />
+                <ActivityCard>
+                    <template #title>
+                        Talent Request
+                    </template>
+                    <template #activity>
+                        <table>
+                            <thead>
+                                <th>Company Name</th>
+                                <th>Status</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        Femi Farewa
+                                    </td>
+                                    <td>
+                                        <span class="text-yellow-500 py-2 px-3">
+                                            Pending
+                                        </span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </template>
+                </ActivityCard>
+                <ActivityCard>
+                    <template #title>
+                        Consultancy Request
+                    </template>
+                    <template #activity>
+                        <table>
+                            <thead>
+                                <th>Company Name</th>
+                                <th>Status</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        Femi Farewa
+                                    </td>
+                                    <td>
+                                        <span class="bg-green-500 text-white py-2 px-3">
+                                            Approved
+                                        </span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </template>
+                </ActivityCard>
+                <ActivityCard>
+                    <template #title>
+                        Freelancers
+                    </template>
+                    <template #activity>
+                        <table>
+                            <thead>
+                                <th>Contact Name</th>
+                                <th>Phone Number</th>
+                                <th>Status</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        Femi Farewa
+                                    </td>
+                                    <td>
+                                        React js Algorithm
+                                    </td>
+                                    <td>
+                                        <span class="bg-yellow-500 text-white py-2 px-3">
+                                            Pending
+                                        </span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </template>
+                </ActivityCard>
+            </div>
+        </div>
+    </div>
+</template>
