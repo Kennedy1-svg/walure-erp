0593221afd0d9d4ec5d8ffe78036f9cfcecc9ab6
Odumz <odumz20@gmail.com>
2022-03-03 18:40:19 +0100

    student list api modification

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,28 +1,5 @@
 <script setup lang="ts">
 import SvgIcons from './SvgIcons.vue';
-import { ref } from 'vue';
-
-const searchText:any = ref('')
-
-const filterSearch:any = () => {
-  const search: any = searchText.value.toLowerCase();
-  const studentList:any = document.getElementById('students');
-  const rows:any = studentList.getElementsByTagName('tr')
-
-  for (let i:number = 0; i < rows.length; i++) {
-    const firstCol:any = rows[i].getElementsByTagName('td')[0];
-    const secondCol:any = rows[i].getElementsByTagName('td')[1];
-    const thirdCol:any = rows[i].getElementsByTagName('td')[2];
-    const fourthCol:any = rows[i].getElementsByTagName('td')[3];
-
-    if (firstCol){
-      const lang:any = firstCol.innerText;
-      const slang:any = secondCol.innerText;
-      const tlang:any = thirdCol.innerText;
-      const flang:any = fourthCol.innerText;
-      rows[i].style.display = ((lang.toLowerCase().indexOf(search) > -1) || (slang.toLowerCase().indexOf(search) > -1) || (tlang.toLowerCase().indexOf(search) > -1) || (flang.toLowerCase().indexOf(search) > -1)) ? '' : 'none';
-    }
-}
 </script>
 
 <template>
