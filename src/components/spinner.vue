a4cd1890c4691db46f62c7fe9f9c08d7023e0e73
Odumz <odumz20@gmail.com>
2022-03-05 01:16:11 +0100

    spinner added to login form

---
+++ src/components/spinner.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<template>
+    <div class="w-5 h-5 border-white animate-spin rounded-full border-2 border-b-transparent"></div>
+</template>
