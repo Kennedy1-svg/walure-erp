fbbfd833aa0f6fc5ef758f4c542cacf7519ef696
Odumz <odumz20@gmail.com>
2022-03-27 17:55:25 +0100

    talent module screens updated

---
+++ src/views/dashboard/instructors/Skill.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { onMounted, reactive } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+    name: 'Skill',
+}
+</script>
+
+<script setup lang="ts">
+import SkillHeader from '../../../components/dashboard/instructors/SkillHeader.vue';
+import SkillList from '../../../components/dashboard/instructors/SkillList.vue';
+</script>
+
+<template>
+    <div class="main mx-9">
+        <SkillHeader />
+        <SkillList />
+    </div>
+</template>
