cdf8b725b5179672fea23b502a44579544c2d049
Odumz <odumz20@gmail.com>
2022-03-27 17:49:44 +0100

    batch module, delete modal component, side nav, edit raw and edit formdata api, router updated

---
+++ src/views/dashboard/instructors/Talent.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { onMounted, reactive } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+    name: 'Talent',
+}
+</script>
+
+<script setup lang="ts">
+import TalentHeader from '../../../components/dashboard/instructors/TalentHeader.vue';
+import TalentList from '../../../components/dashboard/instructors/TalentList.vue';
+</script>
+
+<template>
+    <div class="main mx-9">
+        <TalentHeader />
+        <TalentList />
+    </div>
+</template>
