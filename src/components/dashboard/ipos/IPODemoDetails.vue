4df2c1c90dfeabc719d5c48eff3aff53ed94ab56
Odumz <odumz20@gmail.com>
2022-04-30 16:23:03 +0100

    ipo components updated

---
+++ src/components/dashboard/ipos/IPODemoDetails.vue
@@ -0,0 +1,77 @@
+<script lang="ts">
+export default {
+  name: 'IPODemoDetails',
+}
+</script>
+
+<script setup lang="ts">
+import SvgIcons from '../../SvgIcons.vue';
+import { api_url } from '../../../config'
+import { ref, toRefs, computed, onMounted } from 'vue';
+import { useStore } from 'vuex'
+import moment from 'moment';
+import * as actionTypes from '../../../store/module/ipos/constants/action'
+
+const store = useStore();
+
+const showProfilePicture = ref(false);
+const emits = defineEmits(['close'])
+
+const closeModal:any = () => {
+  // document.getElementById('myoal').showModal()
+  console.log('close modal')
+  emits('close')
+}
+
+const contact:any = computed(() => {
+    // console.log('courses', JSON.parse(JSON.stringify(store.getters.getEditcourse.value)))
+    return store.getters.getEditIPODemo.value
+})
+
+// onMounted(async () => {
+//     console.log('I am now here')
+//     const request:any = `${api_url}api/course/get-courses/{pageIndex}/{pageSize}`;
+//     await store.dispatch(actionTypes.FetchEditcourse, request)
+// })
+
+</script>
+
+<template>
+    <div class="h-[92vh]">
+        <div class="main grid px-[45px]">
+            <div class="grid">
+                <div class="flex justify-between py-10 items-center ">
+                    <p class="text-2xl">Details</p>
+                    <SvgIcons name="cancel" @click="closeModal" class="cursor-pointer" />
+                </div>
+            </div>
+            <!-- {{ contact }} hi -->
+            <div class="bottom">
+                <div class="flex border-b py-3 text-xl font-medium justify-between items-center">
+                    <p>Name</p>
+                    <p>{{ contact.contactName }}</p>
+                </div>
+                <div class="flex border-b py-3 text-xl font-medium justify-between items-center">
+                    <p>Mobile</p>
+                    <p>{{ contact.phoneNumber }}</p>
+                </div>
+                <div class="flex border-b py-3 text-xl font-medium justify-between items-center">
+                    <p>Email</p>
+                    <p>{{ contact.email }}</p>
+                </div>
+                <div class="flex border-b py-3 text-xl font-medium justify-between items-center">
+                    <p>Date</p>
+                    <p>{{ moment(contact.requestDate).format('MM/DD/YYYY') }}</p>
+                </div>
+                <div class="flex gap-5 py-3 text-xl font-medium justify-between items-start">
+                    <p>Company Type</p>
+                    <p>{{ contact.businessType }}</p>
+                    <!-- <p class="min-w-2/3 text-base">
+                    {{ contact.message }}
+                    </p> -->
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
