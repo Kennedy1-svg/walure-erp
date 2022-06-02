e2d2b7d16985d0bc4a46da00a286e25e9353698d
Odumz <odumz20@gmail.com>
2022-06-02 10:12:14 +0100

    role add view updated

---
+++ src/views/dashboard/users/AddRole.vue
@@ -0,0 +1,54 @@
+<script lang="ts">
+
+export default {
+    name: 'Role',
+}
+</script>
+
+<script setup lang="ts">
+import AddRoleHeader from '../../../components/dashboard/users/AddRoleHeader.vue';
+import AddRoleForm from '../../../components/dashboard/users/AddRoleForm.vue';
+import alert from '../../../components/alerts.vue';
+import SvgIcons from '../../../components/SvgIcons.vue';
+import { ref, computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore();
+
+const alertState:any = computed(() => store.getters.getUserAlertStatus.value)
+const alertText:any = computed(() => store.getters.getUserAlertText.value)
+// const batchAlertState:any = computed(() => store.getters.getBatchAlertStatus.value)
+// const batchAlertText:any = computed(() => store.getters.getBatchAlertText.value)
+
+const status:any = computed(() => {
+    let answer:any
+    if (alertText.value.includes('successfully')) {
+        answer = true
+    } else {
+        answer = false
+    }
+    return answer
+})
+</script>
+
+<template>
+    <div class="main mx-9 overflow-hidden">
+        <alert :class="[alertState ? '' : 'hidden']"  class="fixed z-60 top-40 bg-white p-2 right-0" name="result">
+            <template #icon>
+                <p v-if="status" class="bg-green-accent rounded-full border p-2">
+                    <SvgIcons class="text-white" name="tick" />
+                </p>
+                <p v-else class="">
+                    <SvgIcons class="text-red" name="exclamation" />
+                </p>
+            </template>
+            <template #info>
+                <p class="text-sm">
+                    {{ alertText }}
+                </p>
+            </template>
+            <template #button></template>
+        </alert>
+        <AddRoleHeader />
+        <AddRoleForm />
+    </div>
+</template>
