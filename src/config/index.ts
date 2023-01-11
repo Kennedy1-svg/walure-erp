abd8e7d4dc94e824b8827f7063bb1a1d47f614fb
Odumz <odumz20@gmail.com>
2023-01-11 18:20:49 +0100

    config updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,3 +8,14 @@ export const auth_creds:any = import.meta.env.VITE_APP_OIDC_AUTHCREDS;
 export const zoho_client_id:any = import.meta.env.VITE_APP_ZOHO_CLIENT_ID;
 export const zoho_scope:any = import.meta.env.VITE_APP_ZOHO_SCOPE;
 export const base_url:any = import.meta.env.VITE_APP_BASE_URL;
+
+console.log(grant_type)
+console.log(client_id)
+console.log(client_secret)
+console.log(scope)
+console.log(api_url)
+console.log(oidc_authority)
+console.log(auth_creds)
+console.log(zoho_client_id)
+console.log(zoho_scope)
+console.log(base_url)
