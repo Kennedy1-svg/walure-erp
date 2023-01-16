a16763661aa9466c8d8ebdb46f17b3a111a0cab6
Odumz <odumz20@gmail.com>
2023-01-16 12:34:43 +0100

    idsrvauth logout updated for test

--- src/components/TopNavigation.vue
+++ src/components/TopNavigation.vue
@@ -19,15 +19,16 @@ const endSessionEndpoint:any = computed(() => {
 const logout:any = async () => {
 	const token:any = localStorage.getItem('token')
 	// $oidc.signOut
-	await addEmptyData(endSessionEndpoint.value, token)
-	localStorage.clear();
-	window.location.href = '/';
+	let response:any = await addEmptyData(endSessionEndpoint.value, token)
+	console.log(`emergency ${endSessionEndpoint}`)
+	// localStorage.clear();
+	// window.location.href = '/';
 }
 </script>
 
 <template>
 <div class="grid w-4/5 bg-white shadow-md py-2 justify-end items-center px-6">
-	{{ endSessionEndpoint }}
+	<!-- {{ endSessionEndpoint }} -->
 	<div class="relative inline-block text-left dropdown">
 		<button class="border text-grey flex justify-around gap-8 items-center rounded px-3 py-2" type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
 			<span class="icon border rounded-full ">
