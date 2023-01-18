f8eec9fec5ca26db0f1b0b972ecc4131742a93eb
Odumz <odumz20@gmail.com>
2023-01-18 20:03:59 +0100

    idsrvauth logout updated

--- src/components/TopNavigation.vue
+++ src/components/TopNavigation.vue
@@ -1,31 +1,31 @@
 <script lang="ts">
-export default {
-  name: 'TopNavigation',
-}
+	export default {
+	name: 'TopNavigation',
+	}
 </script>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-import SvgIcons from '../components/SvgIcons.vue';
-import { useStore } from 'vuex';
-import { addEmptyData } from '../helpers/api';
-import idsrvAuth from '../idSrvAuth';
-import { createOidcAuth, SignInType, LogLevel } from 'vue-oidc-client/vue3'
-import { zoho_client_id, zoho_scope, base_url, oidc_authority } from '../config'
+	import { computed } from 'vue'
+	import SvgIcons from '../components/SvgIcons.vue';
+	import { useStore } from 'vuex';
+	import { addEmptyData } from '../helpers/api';
+	import idsrvAuth from '../idSrvAuth';
+	import { createOidcAuth, SignInType, LogLevel } from 'vue-oidc-client/vue3'
+	import { zoho_client_id, zoho_scope, base_url, oidc_authority } from '../config'
 
-const loco = window.location
-const appRootUrl = `${loco.protocol}//${loco.host}/`
-// const appRootUrl = 'localhost:5500/'
-const provider:any = 'Zoho'
+	const loco = window.location
+	const appRootUrl = `${loco.protocol}//${loco.host}/`
+	// const appRootUrl = 'localhost:5500/'
+	const provider:any = 'Zoho'
 
-  console.log(`Creating OIDC client for ${appRootUrl}`)
-  const authCallbackPath = 'index.html?auth-callback=1';
-  const logoutCallbackPath = 'index.html?logout-callback=1';
-  const prompt= 'signout'
-  const redirectUri = `${base_url}${authCallbackPath}`;
-  const scopes = 'offline_access';
-  const logoutRedirectUri = `${oidc_authority}${logoutCallbackPath}`;
-  console.log(`Creating OIDC client for ${redirectUri}`)
+	console.log(`Creating OIDC client for ${appRootUrl}`)
+	const authCallbackPath = 'index.html?auth-callback=1';
+	const logoutCallbackPath = 'index.html?logout-callback=1';
+	const prompt= 'signout'
+	const redirectUri = `${base_url}${authCallbackPath}`;
+	const scopes = 'offline_access';
+	const logoutRedirectUri = `${oidc_authority}${logoutCallbackPath}`;
+	console.log(`Creating OIDC client for ${redirectUri}`)
 
 	const clientSettings = {
 	authority: oidc_authority,
@@ -53,6 +53,10 @@ const provider:any = 'Zoho'
 		// let response:any = await addEmptyData(endSessionEndpoint.value, token)
 		console.log(`emergency ${endSessionEndpoint}`)
 		idsrvAuth.signOut(clientSettings);
+		idsrvAuth.events.addUserSignedOut(() => {
+			// eslint-disable-next-line no-console
+			console.log('user signed out')
+		})
 		localStorage.clear();
 		setTimeout(() => {
 			window.location.href = '/';
@@ -62,33 +66,33 @@ const provider:any = 'Zoho'
 </script>
 
 <template>
-<div class="grid w-4/5 bg-white shadow-md py-2 justify-end items-center px-6">
-	<!-- {{ endSessionEndpoint }} -->
-	<div class="relative inline-block text-left dropdown">
-		<button class="border text-grey flex justify-around gap-8 items-center rounded px-3 py-2" type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
-			<span class="icon border rounded-full ">
-				<SvgIcons class="m-2" name="user-avatar" /> <!-- avatar icon -->
-			</span>
-			<span class="text-black">
-				<p class="text-sm">Admin</p>
-				<p class="text-xs">Walure Capital</p>
-			</span>
-			<span class="icon">
-				<SvgIcons name="chevron-down" /> <!-- chevron down icon -->
-			</span>
-		</button>
-		<div class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
-			<div class="absolute right-0 w-full mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none" aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
-				<div class="py-1">
-					<button @click.prevent="logout" tabindex="0" class="flex items-center py-5 px-7"  role="menuitem">
-						<SvgIcons class="mr-4" name="logout" /> <!-- logout icon -->
-						<p>Sign Out</p>
-					</button>
+	<div class="grid w-4/5 bg-white shadow-md py-2 justify-end items-center px-6">
+		<!-- {{ endSessionEndpoint }} -->
+		<div class="relative inline-block text-left dropdown">
+			<button class="border text-grey flex justify-around gap-8 items-center rounded px-3 py-2" type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
+				<span class="icon border rounded-full ">
+					<SvgIcons class="m-2" name="user-avatar" /> <!-- avatar icon -->
+				</span>
+				<span class="text-black">
+					<p class="text-sm">Admin</p>
+					<p class="text-xs">Walure Capital</p>
+				</span>
+				<span class="icon">
+					<SvgIcons name="chevron-down" /> <!-- chevron down icon -->
+				</span>
+			</button>
+			<div class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
+				<div class="absolute right-0 w-full mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none" aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
+					<div class="py-1">
+						<button @click.prevent="logout" tabindex="0" class="flex items-center py-5 px-7"  role="menuitem">
+							<SvgIcons class="mr-4" name="logout" /> <!-- logout icon -->
+							<p>Sign Out</p>
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 </template>
 
 <style scoped>
