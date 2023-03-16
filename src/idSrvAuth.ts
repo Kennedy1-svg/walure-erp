2fcee3b699bbe5481e31c42f88ed729df1c82cdd
0dumz <odumz20@gmail.com>
2023-03-16 16:31:15 +0100

    oidc login updated for prod

--- src/idSrvAuth.ts
+++ src/idSrvAuth.ts
@@ -9,10 +9,10 @@ const appRootUrl = `${loco.protocol}//${loco.host}`
 const provider:any = 'Zoho'
 
   console.log(`Creating OIDC client for ${appRootUrl}`)
-  const authCallbackPath = '/index.html?auth-callback=1';
+  const authCallbackPath = 'index.html?auth-callback=1';
   const logoutCallbackPath = '/index.html?logout-callback=1';
   const prompt= 'login'
-  const redirectUri = `${base_url}${authCallbackPath}`;
+  const redirectUri = `${base_url}/${authCallbackPath}`;
   const scopes = 'offline_access';
   const logoutRedirectUri = `${oidc_authority}${logoutCallbackPath}`;
   console.log(`Creating OIDC client for ${redirectUri}`)
